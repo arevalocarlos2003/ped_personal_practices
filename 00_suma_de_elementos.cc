7ec22380070111d62963a9e70b1e419e51f39094
arevalocarlos2003 <00010423@uca.edu.sv>
2024-08-16 19:48:58 -0600

    Excercise 00

---
+++ 00_suma_de_elementos.cc
@@ -0,0 +1,23 @@
+// Copyright (c) 2024 Carlos Arevalo
+#include <iostream>
+
+int main()
+{
+  int kSize = 5;
+  int number_collection[kSize];
+  std::cout << "Enter five numbers leaving one space between each number:" << std::endl;
+
+  for (int i = 0; i < kSize; ++i)
+  {
+    std::cin >> number_collection[i];
+  }
+
+  int collection_sum = 0;
+
+  for (int i = 0; i < kSize; ++i)
+  {
+    collection_sum = collection_sum + number_collection[i];
+  }
+
+  std::cout << "Number Collection Sum: " << collection_sum << std::endl;
+}
